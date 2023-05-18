c404df60ccb98c45a1f1c77bee1581575b18f0b3
Victor Mameluk <viktormameluk@gmail.com>
2023-05-18 10:54:19 -0300

    configurando app.html

--- jogo.js
+++ jogo.js
@@ -37,7 +37,7 @@ function positionRandom (){
 
 	var mosquito = document.createElement('img')	
 	mosquito.src = 'assets/mosquito.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorioMosquito()
 	mosquito.style.left = positionX + 'px'
 	mosquito.style.top = positionY + 'px'
 	mosquito.style.position = 'absolute'
@@ -45,6 +45,21 @@ function positionRandom (){
 	//Aqui eu acesso o body e adiciono um filho "appendChild"
 	document.body.appendChild(mosquito)
 
+	console.log(tamanhoAleatorioMosquito())
+
+}
+
+function tamanhoAleatorioMosquito(){
+	var classe = Math.floor(Math.random() * 3)
+		
+		switch(classe){
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2: 
+			return 'mosquito3'
+		}
 
 }
 
